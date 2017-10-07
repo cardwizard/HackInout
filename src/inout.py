7471c519b89a218a2a54393a7305dc04dbd01962
Aadesh Bagmar <aadesh@soroco.com>
2017-10-07 14:26:21 +0530

    Added bus numbers to the bus_info api

--- src/inout.py
+++ src/inout.py
@@ -4,11 +4,14 @@ from pathlib import Path
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
+from typing import List
 
 import json
 import hashlib
 import logging
 import binascii
+import random
+import string
 
 # Setup Flask
 app = Flask(__name__)
@@ -17,6 +20,28 @@ app = Flask(__name__)
 
 logging.basicConfig(filename="/opt/soroco/logs/inout.log",
                     level=logging.DEBUG, format='%(asctime)s %(message)s')
+
+def compute_bus_count(frequency: str)->int:
+    frequency_map = {"rare": 2, "very rare": 1, "average": 5, "frequent": 8, "very frequent": 10}
+    max_count = frequency_map.get(frequency.lower(), 1)
+    return random.randint(max(0, max_count-2), max_count)
+
+
+def generate_bus_numbers(count: int)->List:
+    bus_numbers = []
+
+    for item in range(count):
+        number = "KA 01 {}{} {}".format(random.choice(string.ascii_uppercase), random.choice(string.ascii_uppercase),
+                                   random.randint(100, 9999))
+        bus_numbers.append(number)
+    return bus_numbers
+
+
+class BusInfo:
+    def __init__(self, bus_number: int, stops: List[str], bus_frequency: int) -> None:
+        self.bus_number = bus_number
+        self.stops = stops
+        self.bus_frequency = bus_frequency
 
 
 @app.route('/v1/status')
@@ -39,7 +64,6 @@ def bus_info()->jsonify:
     reqparse.add_argument("end", type=str, required=True)
 
     args = reqparse.parse_args(request)
-    print(args.start, args.end)
 
     # Url Params
     url = "https://narasimhadatta.info/cgi-bin/find.cgi"
@@ -49,18 +73,32 @@ def bus_info()->jsonify:
     json = urlopen(request1).read().decode()
     soup = BeautifulSoup(json, 'html.parser')
     tr = soup.find_all('tr')
-    required_bus_info = {}
+
+    required_bus_info = []
 
     for rows in tr:
         td = rows.find_all('td')
-
         if td:
             bus_number = td[0].get_text()
+
             all_stops = td[3].get_text().split(",")
             all_stops = [x.strip() for x in all_stops]
-            required_bus_info[bus_number] = all_stops
+            b = BusInfo(bus_number, all_stops, compute_bus_count(td[4].get_text().strip()))
+            required_bus_info.append(b)
 
-    return required_bus_info
+    result_bus_info = {}
+
+    for item in required_bus_info:
+        buses_data = {}
+        total_buses = generate_bus_numbers(item.bus_frequency)
+
+        for bus in total_buses:
+            buses_data[bus] = item.stops[random.randint(0, item.stops.index(args.start))]
+
+        result_bus_info[item.bus_number] = buses_data
+
+    return jsonify(result_bus_info)
+
 
 @app.route('/v1/all_stops')
 def all_stops()->jsonify:
